2a5938ffbbcbf899352834bd23a3ed7167ae0fad
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-04-27 09:00:59 -0300

    fixed bug G5_ejercicio1

--- guia5/G5_ejercicio1.py
+++ guia5/G5_ejercicio1.py
@@ -7,18 +7,14 @@ from colored import bg
     @github: adrian273
 """
 
-def _loops(number, length):
-    print  bg('blue'), "Los impares siguientes son: "
-    while True:
-        number = number + length
-        if number < 100:
-            print bg('red'),number
-        else:
-            break
-
 number = int(raw_input("Ingrese un numero \n"))
 result = number % 2
 if result == 0:
-    _loops(number, 1)
-else:
-    _loops(number, 2)
+    number = number - 1
+
+while True:
+    number = number + 2
+    if number < 100:
+        print "Los numeros impares son ", number
+    else:
+        break
