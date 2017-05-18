b0e103d4e174a1e7377608ea936ec37095bd5804
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-05-17 21:25:42 -0400

    add AppTaller test

---
+++ python_3/ejerciciosResueltos/AppTaller.py
@@ -0,0 +1,58 @@
+'''
+    @description:
+        La escuela informatica desea tabular la siguiente
+        informacion de cada alumno, con un total de 223:
+            * Nombre, Edad, Peso, Estatura, Genero
+        Con ella realizar los siguientes procesos:
+            * Determinar La Alumna mas joven de la escuela {ok}
+            * Determinar cuantos Alumnos tienen entre 19 y 22 años {ok}
+            * Determinar cuantas Alumnas pesan entre 45 y 55 Kilos {ok}
+            * Quien es el alumno mas alto de la escuela {ok}
+            * porcentaje de mujeres de la escuela
+            * promedio de estatura de la escuela
+    @author: Adrian Verdugo
+'''
+
+
+title = " Escuela Informatica "
+print(title.center(50, '*'))
+line = "Ingreso de datos"
+
+#  Variables
+MIN = 9999
+MAX = 0
+count_students_m = 0
+count_weight_f = 0
+count_f = 0
+
+for i in range(3):
+    print(line.center(50, '_'))
+    name = input('Ingresar nombre \n')
+    age = int(input('Ingresar edad \n'))
+    weight = float(input('Ingresar peso \n'))
+    height = float(input('Ingresar altura \n'))
+    gender = input('Ingresar genero \n')
+    if gender == 'femenino':
+        if MIN > age:
+            # @alumna mas joven de la escuela
+            MIN = age
+            name_min = name
+        if weight > 45 and weight < 55:
+            # @contador de alumnas que pesan entre 45 y 55 kg
+            count_weight_f += 1
+        # @alumnas totales
+        count_f += 1
+    elif gender == 'masculino':
+        if MAX < height:
+            MAX = height
+            # @alumno mas alto
+            name_max_height = name
+        if age > 19 and age < 22:
+            # @contador de alumnos que tienen entre 19 y 22 años
+            count_students_m += 1
+
+
+print('La edad menor es '+ str(MIN) + ' años de la alumna ' + name_min)
+print('La cantidad de alumnos que tienen entre 19 y 22 años son ' + str(count_students_m))
+print('La cantidad de alumnas que pesan entre 45 y 55 kg son ' + str(count_weight_f))
+print('El alumno mas alto de la escuela es ' + name_max_height + ' con ' + str(MAX) + 'cm')
