169c71cc3f7f802b8cb3ac0cc0ff894938589c00
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-06-25 18:55:33 -0400

    update guia_section_2/e15 -> add best_note

--- python_3/guia_python_section_2/ejercicio_15.py
+++ python_3/guia_python_section_2/ejercicio_15.py
@@ -5,7 +5,7 @@
             @Modificar su nota. {ok}
             @Realizar la media de todas las notas. {ok}
             @Realizar la media de las notas menores de 5. {sin terminar}
-            @Mostrar el alumno que mejores notas ha sacado. {sin terminar}
+            @Mostrar el alumno que mejores notas ha sacado. {ok}
             @Mostrar el alumno que peores notas ha sacado. {sin terminar}
 """
 import os
@@ -14,16 +14,14 @@ import os
 COUNT_STUDENT = 20
 data_students = {
                 'adrian': 6.5, 'gonzalo': 4.2, 'andres': 5.6,
-                'juan': 1.3, 'karen': 1.2, 'alice': 7.0, 
+                'juan': 2.3, 'karen': 3.2, 'alice': 7.0, 
                 'maria': 5.5, 'juana': 5.3,
                 'jose': 4.3, 'perez': 2.3, 'gonzales': 5.3, 
-                'peña': 4.3, 'verdugo': 5.3,
-                'escalona': 4.2, 'navarrete': 4.2, 
-                'adasme': 1.3, 'villegas': '4.3', 
-                'elliot': 1.3, 'jovina': 7.0, 'rosa': 6.9
+                'peña': 4.3, 'verdugo': 5.3, 'catalina': 6.0,
+                'escalona': 4.2, 'navarrete': 4.2, 'villegas': 4.3, 
+                'elliot': 1.3, 'jovina': 6.9, 'rosa': 6.7
                 }
 
-print(data_students)
 def search_students():
     name = input('Ingrese nombre a buscar [ingresa -A para mostrar todos]: ')
     if name == '-A':
@@ -50,15 +48,28 @@ def prom_note():
     count = 0
     for i in data_students:
         count += float(data_students[i])
-    return  count / COUNT_STUDENT
+    return  count / len(data_students)
+
+
+def best_note():
+    best_note_student =  0.0
+    for best in data_students:
+        if best_note_student < data_students[best]:
+            best_note_student = data_students[best]
+            name_alumn = best
+    return 'La mejor nota es {} del alumn@ {}'.format(best_note_student, name_alumn)
 
 
 def main():
     options = ['1.. Buscar alumno', '2.. Editar nota',
-                '3.. Media de las notas', '5.. Salir',]
+                '3.. Media de las notas', '5.. Mejor nota', '7.. Salir',]
     while True:
-        title = '\n | Gestion de Notas | \n'
-        print(title.center(80, '-'))
+        print(" _____            _   _                   _        _   _       _            ")
+        print("/ ____|          | | (_)                 | |      | \ | |     | |           ")
+        print("| |  __  ___  ___| |_ _  ___  _ __     __| | ___  |  \| | ___ | |_ __ _ ___ ")
+        print("| | |_ |/ _ \/ __| __| |/ _ \| '_ \   / _` |/ _ \ | . ` |/ _ \| __/ _` / __|")
+        print("| |__| |  __/\__ \ |_| | (_) | | | | | (_| |  __/ | |\  | (_) | || (_| \__ \ ")
+        print(" \_____|\___||___/\__|_|\___/|_| |_|  \__,_|\___| |_| \_|\___/ \__\__,_|___/")
         for op in options:
             print(op)
         try:
@@ -76,6 +87,10 @@ def main():
                 print(title_1.center(80, '-'))
                 print('La media de las notas es {:.1F}'.format(prom_note()))
             elif select == 5:
+                title_1 = '\n | Mejor nota | \n'
+                print(title_1.center(80, '-'))
+                print(best_note())
+            elif select == 7:
                 title_1 = '\n | Adios... | \n'
                 print(title_1.center(80, '-'))
                 break
